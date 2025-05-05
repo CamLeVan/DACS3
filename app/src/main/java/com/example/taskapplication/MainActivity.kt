dd1e5549d8852297b10bdee02daa40efa367fec4
giahuybebu123 <huyphm155@gmail.com>
2025-05-06 02:59:19 +0700

    fix đăng nhập

--- app/src/main/java/com/example/taskapplication/MainActivity.kt
+++ app/src/main/java/com/example/taskapplication/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.example.taskapplication.ui.AppNavigation
